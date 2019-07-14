7e42b4f7934b89badcc27c2c3b1aa40e53ddef94
Vishal Ambavaram <vishal1998@gmail.com>
2019-07-14 18:12:20 -0400

    add features
    js files, work list, social icon bar, nav, scroll back to top etc.

---
+++ js/main.js
@@ -0,0 +1,43 @@
+//jquery
+// $(function() {
+//   console.log("ready");
+
+//   $(".list-group li").click(function(e) {
+//     e.preventDefault();
+
+//     $that = $(this);
+
+//     $(".list-group")
+//       .find("li")
+//       .removeClass("active");
+//     $that.addClass("active");
+//   });
+// });
+//javascript
+// const texts = "Hi! My name is Vishal Ambavaram";
+// let letter = "";
+// let typeWriteIndex = 0;
+// (function type() {
+//   letter = texts.slice(0, ++typeWriteIndex);
+//   document.querySelector(".typing").textContent = letter;
+//   if (typeWriteIndex === texts.length) {
+//   }
+//   setTimeout(type, 150);
+// })();
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("topButton").style.display = "block";
+  } else {
+    document.getElementById("topButton").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
