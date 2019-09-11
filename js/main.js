2db4d2a6f7518f249681bdc39eab7549ae5bcc98
lukeiamyo <vishal1998@gmail.com>
2019-09-11 03:37:44 -0700

    add fade in

--- js/main.js
+++ js/main.js
@@ -59,3 +59,16 @@ function toggleDisappearFunction() {
     document.getElementById("lock-lock").style.display = "block";
   }
 }
+
+document.onreadystatechange = function() {
+  var state = document.readyState;
+  if (state == "interactive") {
+    document.getElementById("contents").style.visibility = "hidden";
+  } else if (state == "complete") {
+    setTimeout(function() {
+      document.getElementById("interactive");
+      document.getElementById("load").style.visibility = "hidden";
+      document.getElementById("contents").style.visibility = "visible";
+    }, 1000);
+  }
+};
