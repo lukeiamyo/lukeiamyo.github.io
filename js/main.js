199497eb4e14c22dd22813ddd2eb3f1a3c820fa3
lukeiamyo <vishal1998@gmail.com>
2020-08-19 22:40:25 -0700

    update readme

--- js/main.js
+++ js/main.js
@@ -1,34 +1,3 @@
-//jquery
-$(document).ready(function() {
-  $(".night-day").click(function() {
-    $("body").toggleClass("dark");
-    $(".name").toggleClass("dark");
-    $(".foot").toggleClass("dark");
-    $(".icon").toggleClass("dark");
-    $(".nav-link").toggleClass("dark");
-    $(".navbar").toggleClass("dark");
-    $(".to-top").toggleClass("dark");
-    $(".card-title").toggleClass("dark");
-    $(".resume:after").toggleClass("dark");
-    $(".card").toggleClass("dark");
-    $(".major").toggleClass("dark");
-    $(".list-group-mine .list-group-item").toggleClass("dark");
-    $(".list-group-mine .list-group-item:hover").toggleClass("dark");
-    $(".list-group-mine .list-group-item:active").toggleClass("dark");
-    $(".list-bar").toggleClass("please");
-    $(".just-head-two").toggleClass("please");
-    $(".rrr").toggleClass("please");
-    $(".nav-link-two").toggleClass("please");
-    $(".navbrdr-two").toggleClass("please");
-    $(".night-day").toggleClass("fa-sun-o fa-moon-o");
-    $(".lock-two").toggleClass("please");
-    $(".date").toggleClass("dark");
-    $(".p-foot").toggleClass("dark");
-    $(".cs61b").toggleClass("dark");
-    $(".card-footer").toggleClass("dark");
-    $(".resume-two-bar").toggleClass("please");
-  });
-});
 
 //javascript
 window.onscroll = function() {
@@ -73,6 +42,14 @@ document.onreadystatechange = function() {
   }
 };
 
-// document.onreadystatechange = function() {
-  
-// };
+
+history.replaceState(null, null, ' ');
+
+
+
+// $(document).ready(function () {
+//   $('a').on("click",function(e){
+//     var url = window.location.toString();
+//     url = url.split("#")[0];
+//   });
+// })
